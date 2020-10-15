d6f40a24834b6366a1133699f6d95ee3fcbc12da
dani-fmena <danif-mena@yandex.com>
2020-10-15 12:40:13 -0500

    Adding a basic transition.

--- src/components/ModalComp.vue
+++ src/components/ModalComp.vue
@@ -1,38 +1,42 @@
 <template>
-    <div class="modal fade"
-         :class="[{'show d-block': show}, {'d-none': !show}, {'modal-mini': modalType === 'mini'}]"
-         v-show="show"
-         tabindex="-1"
-         role="dialog"
-         :aria-hidden="!show">
+    <transition name="fade">
+        <div class="modal fade"
+             :class="[{'show d-block': show}, {'d-none': !show}, {'modal-mini': modalType === 'mini'}]"
+             v-show="show"
+             tabindex="-1"
+             role="dialog"
+             @click.self="handleCloseModal"
+             :aria-hidden="!show">
 
-        <div class="modal-dialog"
-             :class="[{'modal-notice': modalType === 'notice'}, {'modal-dialog-centered': centered}, modalClasses]">
-            <div class="modal-content" :class="[gradient ? `bg-gradient-${gradient}` : '', modalContentClasses]">
+            <div class="modal-dialog"
+                 :class="[{'modal-notice': modalType === 'notice'}, {'modal-dialog-centered': centered}, modalClasses]">
+                <div class="modal-content" :class="[gradient ? `bg-gradient-${gradient}` : '', modalContentClasses]">
 
-                <!-- MODAL HEADER -->
-                <div class="modal-header" :class="[headerClasses]" v-if="$slots.header">
-                    <slot name="header"></slot>
-                    <slot name="close-button">
-                        <button type="button" class="close" v-if="showClose" @click="handleClick" data-dismiss="modal" aria-label="Close">
-                            <i class="tim-icons icon-simple-remove"></i>
-                        </button>
-                    </slot>
+                    <!-- MODAL HEADER -->
+                    <div class="modal-header" :class="[headerClasses]" v-if="$slots.header">
+                        <slot name="header"></slot>
+                        <slot name="close-button">
+                            <button type="button" class="close" v-if="showClose" @click="handleCloseModal"
+                                    data-dismiss="modal" aria-label="Close">
+                                <i class="tim-icons icon-simple-remove"></i>
+                            </button>
+                        </slot>
+                    </div>
+
+                    <!-- MODAL BODY -->
+                    <div v-if="$slots.default" class="modal-body" :class="bodyClasses">
+                        <slot></slot>
+                    </div>
+
+                    <!-- MODAL FOOTER -->
+                    <div v-if="$slots.footer" class="modal-footer" :class="footerClasses">
+                        <slot name="footer"></slot>
+                    </div>
+
                 </div>
-
-                <!-- MODAL BODY -->
-                <div v-if="$slots.default" class="modal-body" :class="bodyClasses">
-                    <slot></slot>
-                </div>
-
-                <!-- MODAL FOOTER -->
-                <div v-if="$slots.footer" class="modal-footer" :class="footerClasses">
-                    <slot name="footer"></slot>
-                </div>
-
             </div>
         </div>
-    </div>
+    </transition>
 </template>
 
 <script lang="ts">
@@ -89,7 +93,7 @@
             },
         },
         methods: {
-            handleClick() {
+            handleCloseModal() {
                 this.$emit("update:show", false)
                 this.$emit("close")
             }
@@ -105,3 +109,12 @@
     })
 </script>
 
+<style lang="scss" scoped>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .8s ease;
+    }
+
+    .fade-enter-from, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
