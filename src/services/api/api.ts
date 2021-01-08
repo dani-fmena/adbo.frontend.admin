38d3a8b63c380021fe15c8a186172c39885f7977
dani-fmena <danif-mena@yandex.com>
2021-01-08 12:12:43 -0500

    Fix 🐞 bug checking the response status code.

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -8,7 +8,7 @@ import { AINVOKER } from '@/store/types/auth/auth-actions-types'
 import { PATHS } from '@/router/paths'
 
 
-Nprogress.configure({ trickleSpeed: 1500, showSpinner: false, easing: 'ease', speed: 500 });
+Nprogress.configure({ trickleSpeed: 1500, showSpinner: false, easing: 'ease', speed: 500 })
 
 const customInstance = axios.create({
     baseURL: config.site.api,
@@ -39,7 +39,7 @@ customInstance.interceptors.response.use(response => {
     
     // Trying to hide the nprogress bar
     const a = document.querySelector('div#topper-border-cont')
-    a!.setAttribute('class', 'main-panel ps mp-stylized');
+    a!.setAttribute('class', 'main-panel ps mp-stylized')
     Nprogress.done()
     
     return response
@@ -48,7 +48,7 @@ customInstance.interceptors.response.use(response => {
     Nprogress.done()
     
     // Handling Expired Token, when token expires, the user will be auth (according tho the state) and the store will have an access token, yet the token will bie invalid for the backend 'cause it's expiration time. So we need to handle this
-    if(error.response.status !== 401) {
+    if (error.response.status === 401) {
         store.dispatch(AINVOKER.LOGOUT)
         router.push(PATHS.login)
     }
